5d4183f01888ca34d4464a0c5489a39bef83fd3a
Jake Miller <jake0miller@gmail.com>
2019-05-17 09:50:49 -0600

    Write four summable methods and benchmark them

---
+++ summable.rb
@@ -0,0 +1,105 @@
+require 'benchmark'
+
+@numbers = [9, 1, -2, 6, 10, -5, -3, -1, -4, 8, 4, 5, 7, 0, 3, 2]
+@numbers = []
+3_000_000.times { @numbers << rand(1..100) }
+
+def find_summing_numbers_with_hash(target)
+  answer_hash = {}
+  @numbers.each do |num|
+    if !answer_hash[num].nil?
+      return [num,answer_hash[num]]
+    else
+      answer_hash[target-num] = num
+    end
+  end
+  return [nil,nil]
+end
+
+p "Hash"
+p find_summing_numbers_with_hash(13)
+p find_summing_numbers_with_hash(16)
+p find_summing_numbers_with_hash(50)
+
+def find_summing_numbers_sort_and_index(target)
+  numbers = @numbers.sort
+  front = 0
+  back = numbers.length-1
+
+  while front < back
+    lil = numbers[front]
+    big = numbers[back]
+    if lil + big == target
+      return [lil,big]
+    elsif lil + big > target
+      back -= 1
+    elsif lil + big < target
+      front += 1
+    end
+  end
+  return [nil,nil]
+end
+
+p "Sort"
+p find_summing_numbers_sort_and_index(13)
+p find_summing_numbers_sort_and_index(16)
+p find_summing_numbers_sort_and_index(50)
+
+def find_summing_numbers_with_double_loop(target)
+  numbers = [9, 1, -2, 6, 10, -5, -3, -1, -4, 8, 4, 5, 7, 0, 3, 2]
+
+  @numbers.each_with_index do |num1,i|
+    @numbers[i+1..-1].each_with_index do |num2,j|
+      return [num1,num2] if num1 + num2 == target
+    end
+  end
+  return [nil,nil]
+end
+
+p "Double loop"
+p find_summing_numbers_with_double_loop(13)
+p find_summing_numbers_with_double_loop(16)
+p find_summing_numbers_with_double_loop(50)
+
+def find_summing_numbers_with_random_sample(target)
+  tried = []
+  while tried.length < @numbers.length
+    num1 = @numbers.sample
+    num2 = @numbers.sample
+    return [num1,num2] if num1 + num2 == target
+    tried << num1
+    tried << num2
+  end
+  return [nil,nil]
+end
+
+p "Random sampling"
+p find_summing_numbers_with_random_sample(13)
+p find_summing_numbers_with_random_sample(16)
+p find_summing_numbers_with_random_sample(50)
+
+Benchmark.bm(12) do |b|
+  b.report("hash") {
+    find_summing_numbers_with_hash(13)
+    find_summing_numbers_with_hash(16)
+    find_summing_numbers_with_hash(50)
+  }
+
+  b.report("sort") {
+    find_summing_numbers_sort_and_index(13)
+    find_summing_numbers_sort_and_index(16)
+    find_summing_numbers_sort_and_index(50)
+  }
+
+  b.report("double loop") {
+    find_summing_numbers_with_double_loop(13)
+    find_summing_numbers_with_double_loop(16)
+    find_summing_numbers_with_double_loop(50)
+  }
+
+  b.report("random sample") {
+    find_summing_numbers_with_random_sample(13)
+    find_summing_numbers_with_random_sample(16)
+    find_summing_numbers_with_random_sample(50)
+  }
+end
